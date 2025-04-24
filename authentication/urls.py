ba4d5586a8e5de19c5923081ce7cd2d94326fcd6
Abdul-Rahman Ibrahim <abdulrahmanibrahim.ish@gmail.com>
2025-04-24 17:46:24 +0300

    added user validation js

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
+from django.views.decorators.csrf import csrf_exempt
 from .views import *
 
 urlpatterns = [
     path('signup/', SignupView.as_view(), name='signup'),
+    path('validate-username/', csrf_exempt(UserNameValidationView.as_view()), name='validate_username'),
 ]
--- authentication/views.py
+++ authentication/views.py
@@ -1,7 +1,23 @@
+import json
 from django.shortcuts import render
+from django.http import JsonResponse
 from django.views import View
+from django.contrib.auth.models import User
 
 class SignupView(View):
     def get(self, request):
         return render(request, 'authentication/signup.html')
+
+
+class UserNameValidationView(View):
+    def post(self, request):
+        data = json.loads(request.body)
+        username = data['username']
+
+        if not str(username).isalnum():
+            return JsonResponse({'username_error': 'username should only contain alphanumeric characters'}, status=400)
+        
+        if User.objects.filter(username=username).exists():
+            return JsonResponse({'username_error': 'sorry username already used'}, status=409)
+        return JsonResponse({'username_valid': True})
     
