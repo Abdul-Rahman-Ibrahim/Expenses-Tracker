d9c92b09ea8a7dbe9abb9c0ae5fced63013191b0
Abdul-Rahman Ibrahim <abdulrahmanibrahim.ish@gmail.com>
2025-05-02 15:03:40 +0300

    expenes addition

--- expenses/models.py
+++ expenses/models.py
@@ -1,3 +1,35 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils.timezone import now
 
-# Create your models here.
+class Expense(models.Model):
+    amount = models.FloatField()
+    date = models.DateField(default=now)
+    description = models.TextField()
+    owner = models.ForeignKey(
+        to=User,
+        on_delete=models.CASCADE
+    )
+
+    category = models.ForeignKey(
+    to='Category',
+    on_delete=models.SET_NULL,
+    null=True,  # Allow expenses to exist even if category is deleted
+    blank=True)
+
+    def __str__(self) -> str:
+        return f"{self.category.name if self.category else 'No Category'}"
+
+    
+    class Meta:
+        ordering = ['-date']
+        
+    
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+
+    class Meta:
+        verbose_name_plural = 'Categories'
+
+    def __str__(self) -> str:
+        return self.name
--- expenses/views.py
+++ expenses/views.py
@@ -1,8 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic import View
 from django.utils.decorators import method_decorator
 from django.contrib.auth.decorators import login_required
 from django.views.decorators.cache import never_cache
+from django.contrib import messages
+
+from .models import Category, Expense
 
 @method_decorator(login_required(login_url='/authentication/login'), name='dispatch')
 @method_decorator(never_cache, name='dispatch')
@@ -15,7 +18,39 @@ class IndexView(View):
 @method_decorator(never_cache, name='dispatch')
 class AddExpenseView(View):
     def get(self, request):
-        return render(request, 'expenses/add_expense.html')
+        
+        context = {
+            'categories': Category.objects.all()
+        }
+
+        # import pdb
+        # pdb.set_trace()
+        return render(request, 'expenses/add_expense.html', context=context)
+    
+    def post(self, request):
+        amount = request.POST.get("amount")
+        description = request.POST.get("description")
+        category_id = request.POST.get("category")
+        date = request.POST.get("expense_date")
+
+        category = Category.objects.get(id=category_id)
+
+        # Create and save new Expense
+        Expense.objects.create(
+            amount=amount,
+            description=description,
+            category=category,
+            owner=request.user,
+            date=date
+        )
+
+        messages.success(request, "Expense added successfully!")
+        return redirect('home')
+
+
+
+
+
 
 # @never_cache
 # @login_required(login_url='/authentication/login')
