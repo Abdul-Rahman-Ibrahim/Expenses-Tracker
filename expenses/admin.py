1835962f18350d2cd16b1012c485cc83dc149a1a
Abdul-Rahman Ibrahim <abdulrahmanibrahim.ish@gmail.com>
2025-06-27 19:46:45 +0300

    customized admin

--- expenses/admin.py
+++ expenses/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 from .models import Category, Expense
 
+class ExpenseAdmin(admin.ModelAdmin):
+    list_display = ('amount', 'description', 'owner', 'category', 'date')
+    search_fields = ('description', 'date', 'owner__username', 'category__name')
+    list_per_page = 5
 
-admin.site.register(Expense)
+admin.site.register(Expense, ExpenseAdmin)
 admin.site.register(Category)
--- income/admin.py
+++ income/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
 from .models import Income, Source
 
-admin.site.register(Income)
+class IncomeAdmin(admin.ModelAdmin):
+        list_display = ('amount', 'description', 'owner', 'source', 'date')
+        search_fields = ('description', 'date', 'owner__username', 'source__name')
+        list_per_page = 5
+
+admin.site.register(Income, IncomeAdmin)
 admin.site.register(Source)
